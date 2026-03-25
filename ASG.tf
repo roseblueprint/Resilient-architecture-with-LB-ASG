2bb296d9fb64d9760c0d4288126bcdb2951e88c2
Roxane <roxanephilippine@gmail.com>
2026-03-25 21:45:38 +0100

    created a load balancer, launch template and autoscalling group

---
+++ ASG.tf
@@ -0,0 +1,16 @@
+resource "aws_autoscaling_group" "asg" {
+  desired_capacity    = 2
+  max_size            = 4
+  min_size            = 2
+  vpc_zone_identifier = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+
+  launch_template {
+    id      = aws_launch_template.lt.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.tg.arn]
+}
